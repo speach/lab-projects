2b64d470cb7dcb90eac46ff8545f3c3ff5533426
Jay Hesselberth <jay.hesselberth@gmail.com>
2014-03-31 09:01:44 -0600

    added pipeline

---
+++ excision-seq-ms/pipeline/config.sh
@@ -0,0 +1,9 @@
+SAMPLES=(6_4.rep1 Afu_BG234_predig CPD.rep1
+         UDG_predig ung1_predig
+         6_4.rep2  Afu_Y7092_predig  CPD.rep2
+         UDG_postdig  ung1_5fu_predig)
+
+PROJECT=$HOME/devel/lab-projects/excision-seq-ms
+FASTQDIR=$PROJECT/ncbi-geo/fastq
+BOWTIEINDEX=$HOME/ref/genomes/sacCer1/sacCer1
+CHROM_SIZES=$HOME/ref/genomes/sacCer1/sacCer1.chrom.sizes
