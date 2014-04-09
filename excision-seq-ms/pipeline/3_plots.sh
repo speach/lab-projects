33b12ef91932cc4f7d99e95204dfefb1337a54ae
Jay Hesselberth <jay.hesselberth@gmail.com>
2014-04-09 09:15:21 -0600

    added min_counts to tables

--- excision-seq-ms/pipeline/3_plots.sh
+++ excision-seq-ms/pipeline/3_plots.sh
@@ -16,36 +16,41 @@ sample=${SAMPLES[$(($LSB_JOBINDEX - 1))]}
 results=$RESULT/$sample
 
 plotdir=$results/plots
-
 if [[ $ASSEMBLY == "sacCer2" ]]; then
-    ignore_modes=("all" "only-mito" "no-mito" "only-2micron")
-    ignore_args=("" "--only-chrom chrM"
-                 "--ignore-chrom chrM"
+    include_modes=("all" "only-mito" "no-mito" "only-2micron")
+    include_args=("" "--only-chrom chrM"
+                 "--include-chrom chrM"
                  "--only-chrom 2micron")
 else
-    ignore_modes=("all" "only-mito" "no-mito")
-    ignore_args=("" "--only-chrom chrM"
-                 "--ignore-chrom chrM")
+    include_modes=("all" "only-mito" "no-mito")
+    include_args=("" "--only-chrom chrM"
+                 "--include-chrom chrM")
 fi
 
-for ig_idx in ${!ignore_modes[@]}; do
+# count thresholds
+count_thresh="1 10 30"
 
-    ignore_mode=${ignore_modes[$ig_idx]}
+for inc_idx in ${!include_modes[@]}; do
+
+    include_mode=${include_modes[$inc_idx]}
 
     # -------------------------------------------------------
     # --- nuc_freq plots ------------------------------------
-    plottypes=("hist" "scatter")
-    for plot_type in ${plottypes[@]}; do
+    for mincount in $count_thresh; do
+        output="$results/$sample.include.$include_mode.mincount.$mincount.nuc_freqs.tab"
 
-        subplotdir="$plotdir/nuc_freqs/$plot_type"
-        if [[ ! -d $subplotdir ]]; then
-            mkdir -p $subplotdir
-        fi
+        plottypes=("hist" "scatter")
+        for plot_type in ${plottypes[@]}; do
 
-        counts="$results/nuc_freqs/$sample.ignore.$ignore_mode.nuc_freqs.tab.gz"
-        sampleid="$sample.subset-$ignore_mode"
-        Rscript --vanilla $RSCRIPTS/nuc.freqs.R $counts "$sampleid" $plot_type $subplotdir
+            subplotdir="$plotdir/nuc_freqs/$plot_type"
+            if [[ ! -d $subplotdir ]]; then
+                mkdir -p $subplotdir
+            fi
 
+            counts="$results/$sample.include.$include_mode.mincount.$mincount.nuc_freqs.tab"
+            sampleid="$sample.subset-$ignore_mode"
+            Rscript --vanilla $RSCRIPTS/nuc.freqs.R $counts "$sampleid" $plot_type $subplotdir
+        done
     done
 done
 
