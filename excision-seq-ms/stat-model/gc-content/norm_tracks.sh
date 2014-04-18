3c2dff482d1b5ef651db761f2543bf1fdc76e2a9
jayhesselberth <jay.hesselberth@gmail.com>
2014-04-18 06:20:53 -0600

    update gc norm

--- excision-seq-ms/stat-model/gc-content/norm_tracks.sh
+++ excision-seq-ms/stat-model/gc-content/norm_tracks.sh
@@ -2,6 +2,8 @@
 
 # make normalized tracks to account for possible GC content bias
 
+source $HOME/devel/lab-projects/excision-seq-ms/pipline/config.sh
+
 window_sizes=(5 50 500)
 signalbg=XXX
 
@@ -16,10 +18,11 @@ for winsize in ${window_sizes[@]}; do
 
     normbg="$sample.win.$winsize.gcnorm.bg"
 
-    bedtools makewindows -g $chromsize -w $winsize \
-        | bedtools nuc -fi $fasta -bed - \
+    bedtools makewindows -g $CHROM_SIZES -w $winsize \
+        | bedtools nuc -fi $FASTA -bed - \
         | awk 'BEGIN {OFS="\t"} {print $1,$2,$3,$5}' \
-        | bedtools map -a - -b $signalbg -c 4 -o mean \
+        | bedtools map -a - -b $signalbg -c 4 -o mean -null 0 \
+        | awk '$5 > 0' \
         | awk 'BEGIN {OFS="\t"} {print $1,$2,$3, $5*$4}' \
         > $normbg
 done
